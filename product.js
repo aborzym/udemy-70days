f738757d78c83495b3c9b9add0251afd69a87212
Andrzej Borzym <borzym.a@gmail.com>
2025-08-30 13:09:06 +0200

    task 04 - 02 - done

--- product.js
+++ product.js
@@ -1,15 +1,33 @@
-//ustawienie litenera na każdy z buttonów
+// Pobranie wszystkich przycisków
+const tabButtons = document.querySelectorAll(".tab button");
 
-document.querySelectorAll(".tab button").forEach((button) => {
-  button.addEventListener("click", () => openTab(ev, tabName));
+// Listener dla każdego przycisku
+tabButtons.forEach((button) => {
+  button.addEventListener("click", (ev) => {
+    const tabName = ev.currentTarget.dataset.target; // pobranie docelowego kontenera
+    openTab(ev, tabName);
+  });
 });
+// Funkcja otwierania zakładki
+const openTab = (ev, tabName) => {
+  // ukryj wszystkie zakładki
+  document
+    .querySelectorAll(".tabcontent")
+    .forEach((tc) => (tc.style.display = "none"));
 
-// ustawienie domyślnej zakładki - opis
+  // usuń klasę active ze wszystkich przycisków
+  document
+    .querySelectorAll(".tab button")
+    .forEach((btn) => btn.classList.remove("active"));
 
+  // pokaż klikniętą zakładkę
+  document.getElementById(tabName).style.display = "block";
+
+  // dodaj active do klikniętego przycisku
+  ev.currentTarget.classList.add("active");
+};
+
+// Kliknięcie domyślne
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("defaultOpen").click();
 });
-
-const openTab = (ev, tabName) {
-    
-}
