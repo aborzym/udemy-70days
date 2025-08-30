0733c55b2798c9be74d45440b974503412a6500b
Andrzej Borzym <borzym.a@gmail.com>
2025-08-30 09:46:43 +0200

    task 03 - 01

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ function togglePlayPause() {
     video.pause();
   }
 }
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".navlinks");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("navactive");
+  burger.classList.toggle("toggle"); // ta klasa aktywuje animację
+});
