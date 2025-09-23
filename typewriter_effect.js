f1a73ad59abf21f7a038ee1e287c6e0af11d0433
Andrzej Borzym <borzym.a@gmail.com>
2025-09-23 09:40:07 +0200

    task 29 done

--- typewriter_effect.js
+++ typewriter_effect.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Miejsce na kod JavaScript
+  const typingEffectElement = document.getElementById("typing-effect");
+  const textToType = `function helloWorld() 
+  {
+    console.log("Hello, world!");
+}
+helloWorld();`;
+  const typingSpeed = 100;
+
+  let index = 0; //sledzi aktualna pozycję w tekście
+
+  //   Utwórz funkcję typeText, która będzie iterować po tekście i dodawać kolejne litery do elementu typingEffectElement.
+  // Jeśli indeks jest mniejszy niż długość tekstu, dodaj aktualny znak do typingEffectElement, zwiększ indeks o 1 i ustaw timeout na wywołanie funkcji typeText ponownie po określonym czasie.
+
+  const typeText = () => {
+    if (index < textToType.length) {
+      typingEffectElement.textContent += textToType.charAt(index);
+      index++;
+      setTimeout(typeText, typingSpeed);
+    }
+  };
+  typeText();
 });
