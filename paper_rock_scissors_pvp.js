08cbf2cef38c6ad9edddfa51ef9b30c4c52971d7
Andrzej Borzym <borzym.a@gmail.com>
2025-09-28 21:40:55 +0200

    task 43 - upgrade01

--- paper_rock_scissors_pvp.js
+++ paper_rock_scissors_pvp.js
@@ -6,21 +6,46 @@ document.addEventListener("DOMContentLoaded", () => {
   const resetBtn = document.querySelector(".reset-btn");
   const drawingBtn = document.getElementById("draw");
   const actionMessage = document.getElementById("action");
-  const player1Message = "Player 1 - make your choice";
-  const player2Message = "Player 2 - make your choice";
+  const playersContainer = document.querySelector(".players-container");
+  const gameContainer = document.querySelector(".game-container");
+  const namesInputs = document.querySelectorAll("input");
+  const p1Name = document.querySelector(".p1-name");
+  const p2Name = document.querySelector(".p2-name");
+  const startBtn = document.querySelector(".play-btn");
+  const changeBtn = document.querySelector(".change-btn");
   let player1Score = 0;
   let player2Score = 0;
   let player1Choice;
   let player2Choice;
   let currentPlayer = 1;
+  let player1Name = "";
+  let player2Name = "";
+  let player1Message = "";
+  let player2Message = "";
 
+  startBtn.addEventListener("click", () => {
+    namesInputs.forEach((input, index) => {
+      if (index === 0) {
+        player1Name = input.value;
+        player1Message = `${player1Name} - make your choice`;
+      }
+      if (index === 1) {
+        player2Name = input.value;
+        player2Message = `${player2Name}  - make your choice`;
+      }
+    });
+    actionMessage.textContent = player1Message;
+    p1Name.textContent = `${player1Name} :`;
+    p2Name.textContent = `${player2Name} :`;
+    playersContainer.classList.add("hidden");
+    gameContainer.classList.remove("hidden");
+  });
   const rules = {
     rock: "scissors",
     scissors: "paper",
     paper: "rock",
   };
 
-  actionMessage.textContent = player1Message;
   choices.forEach((choice) => {
     choice.addEventListener("click", () => {
       if (currentPlayer === 1) {
@@ -48,11 +73,11 @@ document.addEventListener("DOMContentLoaded", () => {
     if (winner === "draw") {
       resultMessage.textContent = `It's a draw! You both chose ${player1Choice}.`;
     } else if (winner === "player1") {
-      resultMessage.textContent = `Player 1 wins! ${capitalizeFirstLetter(
+      resultMessage.textContent = `${player1Name} wins! ${capitalizeFirstLetter(
         player1Choice
       )} beats ${player2Choice}.`;
     } else {
-      resultMessage.textContent = `Player 2 wins ${capitalizeFirstLetter(
+      resultMessage.textContent = `${player2Name}  wins! ${capitalizeFirstLetter(
         player2Choice
       )} beats ${player1Choice}.`;
     }
@@ -82,9 +107,19 @@ document.addEventListener("DOMContentLoaded", () => {
     resultMessage.textContent = "";
     resetScore();
     updateScoreDisplay();
-    actionMessage.textContent = player1Message;
+    actionMessage.textContent = `${player1Name} - make your choice`;
   });
-
+  changeBtn.addEventListener("click", () => {
+    console.log("dupa");
+    resultMessage.textContent = "";
+    resetScore();
+    updateScoreDisplay();
+    gameContainer.classList.add("hidden");
+    playersContainer.classList.remove("hidden");
+    namesInputs.forEach((input) => {
+      input.value = "";
+    });
+  });
   function capitalizeFirstLetter(word) {
     return word[0].toUpperCase() + word.slice(1);
   }
